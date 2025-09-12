945007cac969f941690c99b4476b7774e6b16803
Ysrael Moreno <ysrael.moreno@ab-inbev.com>
2025-09-11 21:37:55 -0300

    first commit

---
+++ src/main/java/com/example/blood_stock_service/BloodStockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.blood_stock_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodStockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodStockServiceApplication.class, args);
+	}
+
+}
